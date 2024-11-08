f0cc9abe8278267a46498b1677302efd8d197ca3
Alegma <6178232+alegmal@users.noreply.github.com>
2024-11-08 22:44:36 +0200

    small re-ordering

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -4,6 +4,34 @@ import (
 	"interpreter/token"
 )
 
+type Lexer struct {
+	input           string
+	currentPosition int  // current position in input (points to current char)
+	nextPosition    int  // current reading position in input (after current char)
+	ch              byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	// initiate properties
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.nextPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.nextPosition]
+	}
+	l.currentPosition = l.nextPosition
+	l.nextPosition += 1
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
@@ -28,33 +56,9 @@ func (l *Lexer) NextToken() token.Token {
 		tok.Literal = ""
 		tok.Type = token.EOF
 	}
+
+	// advance to the next char
 	l.readChar()
+
 	return tok
-}
-
-func newToken(tokenType token.TokenType, ch byte) token.Token {
-	return token.Token{Type: tokenType, Literal: string(ch)}
-}
-
-type Lexer struct {
-	input        string
-	position     int  // current position in input (points to current char)
-	readPosition int  // current reading position in input (after current char)
-	ch           byte // current char under examination
-}
-
-func New(input string) *Lexer {
-	l := &Lexer{input: input}
-	l.readChar()
-	return l
-}
-
-func (l *Lexer) readChar() {
-	if l.readPosition >= len(l.input) {
-		l.ch = 0
-	} else {
-		l.ch = l.input[l.readPosition]
-	}
-	l.position = l.readPosition
-	l.readPosition += 1
 }
